326515341c0a0fb7471335782e866e3b72abf70c
Azharudheen <azharudheenasru@gmail.com>
2024-10-19 17:32:48 +0530

    atlas added

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -7,7 +7,7 @@ const nodemon = require('nodemon');
 
 const connect = async() => {
     try {
-        await mongoose.connect("mongodb://localhost:27017/VibeHunt")
+        await mongoose.connect(process.env.MONGO_URL)
         console.log('mongoDB is connected');
     } catch (error) {
         console.log(error);
